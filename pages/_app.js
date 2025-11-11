a20e28e17e32afa367b0f06d9c73276bd478d58e
dogukanispir <77306909+dogukanispir@users.noreply.github.com>
2025-11-11 16:53:12 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
-import '../styles/globals.css'
+import '@/styles/globals.css'
+import Script from 'next/script'
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-VTBSHMH80C"
+      />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-VTBSHMH80C');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
